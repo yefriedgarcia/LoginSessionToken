cef82462bcb49c6d884c9e5ff44e062a69ddf29a
yefriedgarcia <garciia.h.97@gmail.com>
2021-06-11 08:25:07 -0500

    dev - (api-contacts-motionlayout)

--- app/src/main/java/pt/project/logincontacts/model/repository_contacts/ContactsRepositoryImpl.kt
+++ app/src/main/java/pt/project/logincontacts/model/repository_contacts/ContactsRepositoryImpl.kt
@@ -1,4 +1,6 @@
 package pt.project.logincontacts.model.repository_contacts
+import android.provider.SyncStateContract
+import pt.project.crm_pruebatecnica.constants.Constants
 import pt.project.logincontacts.dtos.contacts_model.ContactsData
 import pt.project.logincontacts.dtos.login_auth_model.Result
 import pt.project.logincontacts.network.APIRequest
@@ -10,6 +12,6 @@ import javax.inject.Singleton
 class ContactsRepositoryImpl @Inject constructor(private val api: APIRequest): ContactsRepository {
 
     override suspend fun getContacts(result: Result):
-            Response<ContactsData> = api.getContacts("query", result.sessionName, "select * from Contacts;")
+            Response<ContactsData> = api.getContacts(Constants.OPERATION_QUERY, result.sessionName, Constants.QUERY)
 
 }
